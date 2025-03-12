a33ebf5b745f2fd295b2003a94f5ce2e863540dd
Valentin BEAUGET <164057524+valentinbgt@users.noreply.github.com>
2025-03-12 11:58:34 +0100

    feat: Added images

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="flex items-center justify-center min-h-screen bg-gray-100">
-    <img src="https://placehold.co/500" alt="" class="rounded-lg shadow-lg" />
+    <img :src="'/img/' + imageList[index]" />
   </div>
 </template>
+
+<script setup>
+const index = ref(1);
+
+const imageList = {
+  1: "1.png",
+  2: "2.png",
+  3: "3.png",
+  4: "4.png",
+  5: "5.png",
+  6: "6.png",
+  7: "7.png",
+  8: "8.png",
+  9: "9.png",
+};
+</script>

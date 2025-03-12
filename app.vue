dd429137ff6c4d363b658edfd4bcb1ca68211670
Valentin BEAUGET <164057524+valentinbgt@users.noreply.github.com>
2025-03-12 11:46:59 +0100

    feat: Added tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <img src="https://placehold.co/500" alt="" class="rounded-lg shadow-lg" />
   </div>
 </template>
